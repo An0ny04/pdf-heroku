7ec40154bbe274fd9cf7d1682c969241af4ed4f7
An0ny04 <An0ny04@github.com>
2021-11-13 21:29:39 +0530

    inital commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,12 +121,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT=os.path.join(BASE_DIR,"media")
-MEDIA_URL="/media/"
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'assets')
+]
+
+if DEBUG == False:
+    STATIC_ROOT =  'static'
+    MEDIA_ROOT = 'media'
+else:
+    STATIC_ROOT =  'static'
+    MEDIA_ROOT = 'media'
